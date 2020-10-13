46282efe6d7b88c8cf0113b8d659d93958366f51
Sean Newman <sxn6296@rit.edu>
2020-10-13 18:09:01 -0400

    Use kebab case for naming

--- networking.tf
+++ networking.tf
@@ -1,16 +1,16 @@
 resource "google_compute_network" "k8s_net" {
-  name = "k8s_net"
+  name = "k8s-net"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "k8s_subnet" {
-  name = "k8s_subnet"
+  name = "k8s-subnet"
   ip_cidr_range = "10.1.0.0/16"
   network = google_compute_network.k8s_net.id
 }
 
 resource "google_compute_firewall" "k8s_allow_internal" {
-  name = "k8s_allow_internal"
+  name = "k8s-allow-internal"
   description = "Allow all traffic internally."
   network = google_compute_network.k8s_net.id
   direction = "EGRESS"
@@ -22,7 +22,7 @@ resource "google_compute_firewall" "k8s_allow_internal" {
 }
 
 resource "google_compute_firewall" "k8s_allow_ingress" {
-  name = "k8s_allow_ingress"
+  name = "k8s-allow-ingress"
   description = "Allow inbound SSH, Kubernetes API, and ICMP traffic from external sources."
   network = google_compute_network.k8s_net.id
   direction = "INGRESS"
@@ -39,5 +39,5 @@ resource "google_compute_firewall" "k8s_allow_ingress" {
 }
 
 resource "google_compute_address" "k8s_address" {
-  name = "k8s_address"
+  name = "k8s-address"
 }
